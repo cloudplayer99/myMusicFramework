3e32cdd55defa705789db18375d27fcdc742bb8c
Cloud_Player <newphilosophy1504@gmail.com>
2022-01-14 18:07:25 +0800

    Implemented Skeleton for custom slider visuals

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,13 +13,53 @@
 
 /********************************* my code here ***************************************/
 
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
-    {
+    void drawRotarySlider(juce::Graphics&,
+                          int x, int y, int width, int height,
+                          float sliderPosProportional,
+                          float rotaryStartAngle,
+                          float rotaryEndAngle,
+                          juce::Slider&) override { }
+};
 
+//struct CustomRotarySlider : juce::Slider
+//{
+//    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+//                                        juce::Slider::TextEntryBoxPosition::NoTextBox)
+//    {
+//
+//    }
+//};
+// want to change the Slider class
+// LookAndFeel
+// sophisticated!
+// rename CustomRotarySlider -> RotarySliderWithLabels
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : 
+    juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                 juce::Slider::TextEntryBoxPosition::NoTextBox),
+    param(&rap),
+    suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
     }
+
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    void paint(juce::Graphics& g) override { }
+    juce::Rectangle<int> getSliderbounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString() const;
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 // the components should not draw outside its bounds
@@ -81,7 +121,7 @@ private:
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
 
-    CustomRotarySlider
+    RotarySliderWithLabels
         peakFreqSlider,
         peakGainSlider,
         peakQualitySlider,
